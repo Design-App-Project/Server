6b0ac57c495f04d6da52509c117f8562aa527552
GeomJa <52377043+donut0310@users.noreply.github.com>
2021-02-03 23:29:10 +0900

    cors관련 헤더 허용

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,14 @@ new Models().init();
 
 app.use(express.json({ limit: "5mb" }));
 
+// cors config
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "content-type, access-token"); //1
+  next();
+});
+
 //route
 routes.push(new CompanyRoute(app));
 
