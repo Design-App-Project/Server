c8052e6ef53ca2bc85da6a35bb35b615d0bc168a
GeomJa <52377043+donut0310@users.noreply.github.com>
2021-03-13 19:07:33 +0900

    관리자페이지 업체 추가, 삭제 api 개발

--- src/api/controllers/company.controller.ts
+++ src/api/controllers/company.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
-import { CompanyModel, ICompany } from "../models/company.model";
+import { CompanyModel, ICompany, Company } from "../models/company.model";
+import { ErrorUtil } from "../utils/error.util";
+import { ResponseUtil } from "../utils/response.util";
 
 export class CompanyController {
   private companyModel: CompanyModel;
@@ -7,47 +9,49 @@ export class CompanyController {
     this.companyModel = new CompanyModel();
   }
 
-  createCompany = async (req: Request, res: Response) => {
-    await this.companyModel.create(req.body);
-
-    res.status(200).send({
-      success: true,
-      message: "",
-      results: {},
-    });
-  };
-
   getAllData = async (req: Request, res: Response) => {
     const data: ICompany = await this.companyModel.readAllData("");
 
     if (data) {
-      res.status(200).json({
-        success: true,
-        result: data,
-      });
+      res.status(200).send(ResponseUtil.successTrue(data, ""));
     } else {
-      res.status(404).send({
-        success: false,
-        messgae: "Not found",
-        result: null,
-      });
+      const error = ErrorUtil.badRequest(``, `Not found`);
+      res.status(error.status).send(ResponseUtil.successFalse(error));
     }
   };
 
   getFilteredData = async (req: Request, res: Response) => {
     const data: ICompany = await this.companyModel.readFilteredData(req.body);
     if (data[0]) {
-      res.status(200).send({
-        success: true,
-        result: data,
-      });
+      res.status(200).send(ResponseUtil.successTrue(data, ""));
     } else {
-      res.status(404).send({
-        success: false,
-        messgae: "Not found",
-        result: null,
-        errors: false,
-      });
+      const error = ErrorUtil.badRequest(``, `Not found`);
+      res.status(error.status).send(ResponseUtil.successFalse(error));
+    }
+  };
+
+  // **관리자 페이지 API** //
+
+  // 업체 추가 api
+  addCompany = async (req: Request, res: Response) => {
+    await this.companyModel.create(req.body);
+
+    res.status(200).send(ResponseUtil.successTrue({}, ""));
+  };
+
+  // 업체 삭제 api
+  removeCompany = async (req: Request, res: Response) => {
+    const company: ICompany = await Company.findOneAndDelete({
+      title: req.body.title,
+    });
+
+    if (company) {
+      res
+        .status(200)
+        .send(ResponseUtil.successTrue({}, "삭제가 완료되었습니다."));
+    } else {
+      const error = ErrorUtil.badRequest(``, `미등록된 업체입니다.`);
+      res.status(error.status).send(ResponseUtil.successFalse(error));
     }
   };
 }
--- src/api/routes/v1/company.route.ts
+++ src/api/routes/v1/company.route.ts
@@ -1,5 +1,6 @@
 import { Application } from "express";
 import { CompanyController } from "../../controllers/company.controller";
+import { AuthMiddleware } from "./../../middlewares/auth.middleware";
 
 export class CompanyRoute {
   private app: Application;
@@ -11,8 +12,16 @@ export class CompanyRoute {
 
   configure() {
     const companyController = new CompanyController();
-    // this.app.post("/api/v1/test-input", [companyController.createCompanies]);
+    const authMiddleware = new AuthMiddleware();
+
+    this.app.post("/api/v1/test-input", [companyController.addCompany]);
+
     this.app.get("/api/v1/material", [companyController.getAllData]);
+
+    this.app.post("/api/v1/material", [companyController.addCompany]);
+
+    this.app.delete("/api/v1/material", [companyController.removeCompany]);
+
     this.app.post("/api/v1/filtering", [companyController.getFilteredData]);
   }
 }
